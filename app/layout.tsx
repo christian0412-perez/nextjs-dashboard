c519dc6bcce2fc0b32a24a07a0aa236db11a0046
Christian Alonso Perez Trujillo <pichito0412@gmail.com>
2023-12-20 15:56:30 -0800

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter , lusitana,roboto, rubik ,rubik2} from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+            <body className={`${rubik2.className} antialiased`}>{children}</body>
+
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter,Lusitana, Roboto, Rubik ,Diphylleia} from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'],weight:['400','700'] });
+export const roboto = Roboto({ subsets: ['latin'],weight:['400','700'] });
+export const rubik = Rubik({ subsets: ['latin'],weight:['400','700'] });
+export const rubik2 = Diphylleia({ subsets: ['latin'],weight:['400','400'] });
